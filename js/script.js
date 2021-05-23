7234a5998d6cd9530d5c1d7ffea62f71754223b6
Jailton Mendes da Silva <jailton.mendes@interne.com.br>
2021-05-23 02:42:23 -0300

    Paginas

---
+++ js/script.js
@@ -0,0 +1,19 @@
+  function imgSlider(anything) {
+        document.querySelector('.pepsi').src = anything;
+    }
+
+    function changeBgcolor(color){
+        const sec = document.querySelector('.sec');
+        sec.style.background = color;
+    }
+
+    function titulo(titulos){
+        const titulo =  document.querySelector('.titulo');
+        titulo.style.color = titulos;
+        
+    }
+
+    function subtitulo(sub){
+        const subtitulo = document.querySelector('.subtitulo');
+        subtitulo.style.color = sub;
+    }

c54df82546547c81f83633920d524afc1b557bb6
Jailton Mendes da Silva <jailton.mendes@interne.com.br>
2021-05-27 00:34:57 -0300

    final parte 1

--- js/script.js
+++ js/script.js
@@ -1,19 +1,41 @@
   function imgSlider(anything) {
         document.querySelector('.pepsi').src = anything;
     }
+    
 
     function changeBgcolor(color){
         const sec = document.querySelector('.sec');
         sec.style.background = color;
     }
 
+
     function titulo(titulos){
+
         const titulo =  document.querySelector('.titulo');
         titulo.style.color = titulos;
         
     }
 
+
+    function time(times){
+    
+        const time = document.querySelector('.titulo');
+        time.textContent = times;
+    }
+
+
     function subtitulo(sub){
         const subtitulo = document.querySelector('.subtitulo');
         subtitulo.style.color = sub;
+    }
+
+    function subtituloText(text){
+        const subtituloText = document.querySelector('.subtitulo');
+        subtituloText.textContent = text;
+    }
+
+
+    function saiba(saibaMais){
+        const fundo = document.querySelector('.saibaMais');
+        fundo.style.background = saibaMais;
     }
